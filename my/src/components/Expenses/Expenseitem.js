2d432059d8234e307c8db37ecb638b1e46e2899f
vaibhav <vaibhavsachdeva2000@gmail.com>
2022-07-04 09:44:36 +0530

    usestate added

--- my/src/components/Expenses/Expenseitem.js
+++ my/src/components/Expenses/Expenseitem.js
@@ -2,16 +2,31 @@ import React from 'react'
 import './Expenseitem.css'
 import Expensedate from './Expensedate'
 import Card from '../UI/Card'
+import {useState} from 'react';
 
+
+// here title is not upadted initially because the componenet is intially
+// rendered first time , not rendered again to show the changes
 function Expenseitem(props) {
+
+
+    const [title , settitle] = useState(props.title);
+
+    function clickhandler(events)
+    {
+        settitle("updatee");
+    }
+
+    
 
 
     return <Card className = 'expense-item'>
             <Expensedate date  = {props.date}/>
         <div className='expense-item__description'>
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className='expense-item__price'>${props.amount}</div>
         </div>
+        <button onClick = {clickhandler}>Change Title</button>
         </Card>
 }
 
